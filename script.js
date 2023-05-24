cbadbda7a8346cbadd14d07d0d9d59404a27eb37
ScavaMS <marinaschiapparr@gmail.com>
2023-05-24 17:28:44 -0300

    página 2.0

--- script.js
+++ script.js
@@ -1,17 +1,98 @@
+document.querySelector(".button-menu-toggle").addEventListener("click", function() {
+      document.querySelector(".nav-links").classList.toggle("nav-links-responsive");
+  });
+  
+
 function showAlert() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
 
+
+  const aa = ["audio/Sera.mp3", "audio/Lost-Elf.mp3", "audio/Main-Theme.mp3", "audio/Calling-You-A-Liar.mp3", "audio/Rick.mp3", "audio/Hawke-theme.mp3", "audio/Rogue-Heart.mp3", "audio/Grey-Warden.mp3", "audio/Orlais-Theme.mp3", "audio/The-One.mp3", "audio/Tavern.mp3", "audio/Dawn.mp3", "audio/Leliana.mp3"];  
+  let cr = 1;
+
+  function myFunction() {
+    document.getElementById("cancion").src= aa[cr];
+    document.getElementById("canciones").load();
+    cr = Math.floor(Math.random() * aa.length);
+    }
+
+    function Liar() {
+      document.getElementById("cancion").src= aa[3];
+      document.getElementById("canciones").load();
+    }
+
+    function Dawn() {
+      document.getElementById("cancion").src= aa[11];
+      document.getElementById("canciones").load();
+    }
+
+    function Warden() {
+      document.getElementById("cancion").src= aa[7];
+      document.getElementById("canciones").load();
+    }
+
+    function Hawke() {
+      document.getElementById("cancion").src= aa[5];
+      document.getElementById("canciones").load();
+    }
+
+    function Leliana() {
+      document.getElementById("cancion").src= aa[12];
+      document.getElementById("canciones").load();
+    }
+
+    function Elf() {
+      document.getElementById("cancion").src= aa[1];
+      document.getElementById("canciones").load();
+    }
+
+    function Main() {
+      document.getElementById("cancion").src= aa[2];
+      document.getElementById("canciones").load();
+    }
+
+
+    function Orlais() {
+      document.getElementById("cancion").src= aa[8];
+      document.getElementById("canciones").load();
+    }
+   
+    function Rogue() {
+      document.getElementById("cancion").src= aa[6];
+      document.getElementById("canciones").load();
+    }
+
+    function Cancion() {
+      document.getElementById("cancion").src= aa[4];
+      document.getElementById("canciones").load();
+    }
+
+    function Sera() {
+      document.getElementById("cancion").src= aa[0];
+      document.getElementById("canciones").load();
+    }
+
+    function Tavern() {
+      document.getElementById("cancion").src= aa[10];
+      document.getElementById("canciones").load();
+    }
+
+    function One() {
+      document.getElementById("cancion").src= aa[9];
+      document.getElementById("canciones").load();
+    }
+
+
+    
 const form = document.getElementById('myForm');
 form.addEventListener('submit', function(event) {
   event.preventDefault();
   validateForm();
 });
 
-
 function validateRespuesta(respuesta) {
     const regex = /^si$/i;
     return regex.test(respuesta);
   }
-  
   function validateForm() {
     const respuestaInput = document.getElementById('respuesta');
     const respuesta = respuestaInput.value;
@@ -21,89 +102,3 @@ function validateRespuesta(respuesta) {
       alert("Que intelectual por dios.");
     }
   }
-
-  const aa = ["audio/Sera.mp3", "audio/Lost-Elf.mp3", "audio/Main-Theme.mp3", "audio/Calling-You-A-Liar.mp3", "audio/Rick.mp3", "audio/Hawke-theme.mp3", "audio/Rogue-Heart.mp3", "audio/Grey-Warden.mp3", "audio/Orlais-Theme.mp3", "audio/The-One.mp3", "audio/Tavern.mp3", "audio/Dawn.mp3", "audio/Leliana.mp3"];  
-  let cr = 1;
-
-  function myFunction() {
-    document.getElementById("Sera").src= aa[cr];
-    document.getElementById("cancione").load();
-    cr = Math.floor(Math.random() * aa.length);
-
-    }
-
-
-
-
-    document.querySelector("button.button-menu-toggle")
-    .addEventListener("click", function() {
-    document.querySelector(".nav-links").
-    classList.toggle("nav-links-responsive")})
-    
-
-
-    function Liar() {
-      document.getElementById("Sera").src= aa[3];
-      document.getElementById("cancione").load();
-    }
-
-    function Dawn() {
-      document.getElementById("Sera").src= aa[11];
-      document.getElementById("cancione").load();
-    }
-
-    function Warden() {
-      document.getElementById("Sera").src= aa[7];
-      document.getElementById("cancione").load();
-    }
-
-    function Hawke() {
-      document.getElementById("Sera").src= aa[5];
-      document.getElementById("cancione").load();
-    }
-
-    function Leliana() {
-      document.getElementById("Sera").src= aa[12];
-      document.getElementById("cancione").load();
-    }
-
-    function Elf() {
-      document.getElementById("Sera").src= aa[1];
-      document.getElementById("cancione").load();
-    }
-
-    function Main() {
-      document.getElementById("Sera").src= aa[2];
-      document.getElementById("cancione").load();
-    }
-
-
-    function Orlais() {
-      document.getElementById("Sera").src= aa[8];
-      document.getElementById("cancione").load();
-    }
-   
-    function Rogue() {
-      document.getElementById("Sera").src= aa[6];
-      document.getElementById("cancione").load();
-    }
-
-    function Cancion() {
-      document.getElementById("Sera").src= aa[4];
-      document.getElementById("cancione").load();
-    }
-
-    function Sera() {
-      document.getElementById("Sera").src= aa[0];
-      document.getElementById("cancione").load();
-    }
-
-    function Tavern() {
-      document.getElementById("Sera").src= aa[10];
-      document.getElementById("cancione").load();
-    }
-
-    function One() {
-      document.getElementById("Sera").src= aa[9];
-      document.getElementById("cancione").load();
-    }

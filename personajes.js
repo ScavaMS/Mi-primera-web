6f278f95c18618f54e26c16630231b12e5f352cb
ScavaMS <marinaschiapparr@gmail.com>
2023-05-29 17:51:27 -0300

    Página 3.5

--- personajes.js
+++ personajes.js
@@ -18,58 +18,58 @@ function wynne() {alert("Abuela poseída (no viene del juego Granny)");}
 
 function zevran() {alert("Intentó matar al protagonista pero le chamuyó así que ahora puede vivir");}
 
-function anders() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function anders() {alert("Un mago que tiene un sentido de la JUSTICIA extremo. ¿Qué tan mal podría acabar? (ver: Dragon Age 2)");}
 
-function justice() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function justice() {alert("Un espíritu bueno (no)");}
 
-function nathaniel() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function nathaniel() {alert("Hijo de uno de los antagonistas pero no pasa nada porque es bueno y gracioso.");}
 
-function oghren2() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function oghren2() {alert("Qué bueno ver a Oghren como compañero otra vez (dijo nadie nunca)");}
 
-function sigrun() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function sigrun() {alert("Enana buena onda con un buen sentido del humor. Lo que Oghren tuvo que haber sido");}
 
-function velanna() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function velanna() {alert("Cometió genocidio pensando que habían secuestrado a su hermana. Éso te dice todo");}
 
-function aveline() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function aveline() {alert("Policía mala que es corrupta con tal de no poner a todo el equipo en prisión");}
 
-function carver() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function carver() {alert("El mellizo malo.");}
 
-function anders2() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function anders2() {alert("Anders: la secuela ¡Ahora poseído por el espíritu de Justicia!");}
 
-function fenris() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function fenris() {alert("Elfo magofóbico que sería fan de My Chemical Romance");}
 
-function isabela() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function isabela() {alert("Pirata con miedo al compromiso que le gusta guardar secretos");}
 
-function bethany() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function bethany() {alert("La melliza buena.");}
 
-function merill() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function merill() {alert("Casi comete genocidio ante todo su clan por despistada (es un poco torpe nada más)");}
 
-function sebastian() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function sebastian() {alert("Chico religioso que no puede estar un minuto sin recitar un verso o meterse en la vida de los demás");}
 
-function tallis() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function tallis() {alert("¿Quién?");}
 
-function varric() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function varric() {alert("Narrador poco fiable que le gusta escribir sobre las vidas personales de otros");}
 
-function blackwall() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function blackwall() {alert("Un guarda gris promedio... ¿No? ¿¿No??");}
 
-function bull() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function bull() {alert("Disfruta un poco mucho pelear contra dragones y estar al borde de morir...");}
 
-function cassandra() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function cassandra() {alert("Fan del autor que ella misma secuestró para que le contara personalmente un libro que él publicó (definitivamente sería fan de Backstreet Boys");}
 
-function cole() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function cole() {alert("Otro espíritu totalmente amigable. No, en serio. Éste si es de los buenos");}
 
-function cullen() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function cullen() {alert("Ah si, siempre tiene que estar el que tuvo un pasado oscuro que está intentando redimirse pero sigue teniendo adicciones");}
 
-function dorian() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function dorian() {alert("Mago de Tevinter que decide ignorar todas sus problemáticas pero no pasa nada porque al menos tiene estilo");}
 
-function josephine() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function josephine() {alert("Posiblemente la única del equipo que no ha cometido ningún crimen... Menos alterar documentos políticos");}
 
-function leliana2() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function leliana2() {alert("Monja que le gusta apuñalar a la gente pero ahora se tomó las pastillas, y pasó de ser esquizofrénica a ser una chica misteriosa (nunca había sentido tanto por tremendo desarrollo de personaje)");}
 
-function sera() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function sera() {alert("Mitad de las cosas que dice no tienen sentido pero es muy graciosa");}
 
-function solas() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function solas() {alert("Tiene un lobo como fursona. Auuuuu");}
 
-function varric2() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function varric2() {alert("Varric de nuevo. Sólo Varric. No cambió nada pero no hacía falta cambiar la perfección");}
 
-function vivienne() {alert("Descargate el Dragon Age 2 ya o te filtro la IP");}
+function vivienne() {alert("Juzga a todos por igual. Nadie se salva");}
